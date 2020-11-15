95749a2ea92a1f2931c59e87eb4e990de3c4f291
momo1093324 <71679878+momo1093324@users.noreply.github.com>
2020-11-15 15:19:59 +0800

    做完 差打包與確認

---
+++ 2D_Midterm_107051005/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void NextLeve1(string nameLv)
+    {
+        SceneManager.LoadScene("nameLv");
+    }
+    public void BackToMune  ()
+    {
+        SceneManager.LoadScene("初始選單");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ 2D_Midterm_107051005/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public GameObject fianal;
+    //道具數量
+    public Text textCount;
+    //數量
+    public int count;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //print("碰到傳送門");
+        if (collision.name=="傳送門")
+        {
+        fianal.SetActive(true);
+        }
+        if (collision.tag == "鑽石")
+        {
+            Destroy(collision.gameObject);
+            count++;
+            textCount.text = "鑽石數量：" + count;
+        }
+    }
+
+}
